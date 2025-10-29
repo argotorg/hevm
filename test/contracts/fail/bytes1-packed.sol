27da2066af2e4a1357d9a5bc96f1285e600971f4
Mate Soos <mate.soos@argot.org>
2025-10-29 17:21:15 +0100

    Adding explanation
    Update
    
    Update

--- test/contracts/fail/bytes1-packed.sol
+++ test/contracts/fail/bytes1-packed.sol
@@ -23,6 +23,15 @@ contract C {
       mstore(0x40, add(dataPtr, dataLen))
 
       // Perform the low-level call
+      // this should call the inc() function, which has selector:
+      //```
+      //cast keccak "inc()"
+      //0x371303c051bff726100ad13871cababf50c20dd920fca137e519f98f089a74b4
+      //```
+      // which happens to have the first 4 bytes as
+      // 0x37, 0x13 0x03, 0xc0
+      // This will increment `state` by 1, thereby violating the property
+
       success := call(gas(), target, 0, dataPtr, dataLen, 0, 0)
     }
 
