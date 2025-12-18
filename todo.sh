f93250b7611d7df5d3aa131fce1705d6118f8e14
Mate Soos <mate.soos@argot.org>
2025-12-18 13:43:52 +0100

    Update

--- todo.sh
+++ todo.sh
@@ -29,3 +29,11 @@ It will fail on data-dword. Just go into vendored/
 
 And issue first in data-bword and then in data-dword:
 cabal --with-compiler=wasm32-wasi-ghc --with-hc-pkg=wasm32-wasi-ghc-pkg v2-build . --disable-profiling -v
+
+
+how it's saying:
+
+Done! I've commented out the -threaded and -with-rtsopts=-N options in hevm.cabal:202 since these are not compatible with WASM builds.
+
+
+cabal --with-compiler=wasm32-wasi-ghc --with-hc-pkg=wasm32-wasi-ghc-pkg --with-hsc2hs=wasm32-wasi-hsc2hs v2-build --allow-newer=base,ghc-bignum,template-haskell --constraint="hashable>=1.5.0.0" --disable-tests --enable-library-vanilla --disable-library-for-ghci -j1 exe:hevm
