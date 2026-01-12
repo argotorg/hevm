93d5a97d0209b788f692c1e067889d14ecae5be5
Mate Soos <mate.soos@argot.org>
2026-01-12 16:53:56 +0100

    Fixing build

--- test/test.hs
+++ test/test.hs
@@ -9,6 +9,7 @@ import Prelude hiding (LT, GT)
 
 import GHC.TypeLits
 import Data.Proxy
+import Control.Concurrent.STM.TVar (newTVarIO)
 import Control.Monad
 import Control.Monad.ST (stToIO)
 import Control.Monad.State.Strict
@@ -1995,7 +1996,8 @@ tests = testGroup "hevm"
           let calldata = (WriteWord (Lit 0x0) (Var "u") (ConcreteBuf ""), [])
           initVM <- liftIO $ stToIO $ abstractVM calldata initCode Nothing True
           let iterConf = IterConfig {maxIter=Nothing, askSmtIters=1, loopHeuristic=StackBased }
-          paths <- interpret (Fetch.noRpcFetcher s) iterConf initVM runExpr pure
+          shouldAbort <- liftIO $ newTVarIO False
+          paths <- interpret (Fetch.noRpcFetcher s) iterConf initVM shouldAbort runExpr pure
           let exprSimp = map Expr.simplify paths
           assertBoolM "unexptected partial execution" (not $ any isPartial exprSimp)
     , test "mixed-concrete-symbolic-args" $ do
@@ -2087,7 +2089,8 @@ tests = testGroup "hevm"
         withDefaultSolver $ \s -> do
           vm <- liftIO $ stToIO $ loadSymVM runtimecode (Lit 0) initCode False
           let iterConf = IterConfig {maxIter=Nothing, askSmtIters=1, loopHeuristic=StackBased }
-          paths <- interpret (Fetch.noRpcFetcher s) iterConf vm runExpr pure
+          shouldAbort <- liftIO $ newTVarIO False
+          paths <- interpret (Fetch.noRpcFetcher s) iterConf vm shouldAbort runExpr pure
           let exprSimp = map Expr.simplify paths
           assertBoolM "expected partial execution" (any isPartial exprSimp)
     ]
