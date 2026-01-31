22beeee44ebc0efd03a90c9b42c851f013bf9e69
gustavo-grieco <gustavo.grieco+github@gmail.com>
2026-01-31 20:20:11 +0100

    fixes

--- test/contracts/pass/math.sol
+++ test/contracts/pass/math.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity >=0.8.0 <0.9.0;
 
+import {Test} from "forge-std/Test.sol";
+
 /// Adapted from halmos tests/solver/test/Math.t.sol
 /// Tests average computation equivalence
-contract MathTest {
+contract MathTest is Test {
     function prove_Avg(uint a, uint b) public pure {
+        require(a + b >= a); // no overflow
         unchecked {
             uint r1 = (a & b) + (a ^ b) / 2;
             uint r2 = (a + b) / 2;
