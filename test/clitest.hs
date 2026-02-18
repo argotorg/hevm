a4ef47e376a289487c12864f96abe8567818ebf7
Mate Soos <mate.soos@argot.org>
2026-02-18 15:23:36 +0100

    Fixing this test

--- test/clitest.hs
+++ test/clitest.hs
@@ -198,8 +198,9 @@ main = do
         shouldBe fileExists True
         removeFile filename
       it "early-abort" $ do
-        (exitcode, stdout, stderr) <- runForge "test/contracts/pass/early-abort.sol" ["--max-iterations", "1000"]
-        putStrLn $ "Exit code: " ++ show exitcode
+        (_, stdout, _) <- runForge "test/contracts/pass/early-abort.sol" ["--max-iterations", "1000"]
+        stdout `shouldContain` "[FAIL]"
+        (T.count "Counterexample:" (T.pack stdout)) `shouldBe` 9
       it "rpc-cache" $ do
         (_, stdout, stderr) <- runForge "test/contracts/fail/rpc-test.sol"
           ["--rpc", "http://mock.mock", "--prefix", "test_attack_symbolic"
